4607909316739fb156c3ea2c4955a9beee852cbb
Martez Carr <113547475+CodingCowboy30@users.noreply.github.com>
2023-10-16 21:48:21 -0500

    Card Skeleton built for object placement

--- js/components/customerReviewCard/index.js
+++ js/components/customerReviewCard/index.js
@@ -1,1 +1,25 @@
-// intial commit
+const customerReviewCard = () => {
+  return  `<div> 
+              <div>
+                  <div>
+                     <div>Stars</div>
+                      <div>
+                         <div>
+                            <h3></h3>
+                            <img>
+                            <p></p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+         </div>`
+}
+
+// THIS IS A SKELETON BREAKDOWN
+
+// Line 2 & 14 is Main Card Box
+// Line 3 & 13 is Container box for all Info
+// Line 4 & 12 is Container to hold Visible Contents
+// Line 5 is where Stars will be 
+// Line 6 & 11 will hold text for Name and review with Verified Check image
+// Line 10 is where the reveiw text will go
