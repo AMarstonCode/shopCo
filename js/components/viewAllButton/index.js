cbbda3431d70913d6d786afd911703c97f515b7a
BVenablecoding <139520221+BVenablecoding@users.noreply.github.com>
2023-10-17 20:03:05 -0500

    I created a view all button

--- js/components/viewAllButton/index.js
+++ js/components/viewAllButton/index.js
@@ -1,1 +1,13 @@
-// initil comment
+function viewAllButton() {
+    const button = document.createElement('button');
+    button.className = 'view-all-button';
+    button.textContent = 'View All';
+    return button.outerHTML;
+  }
+  const myViewAllButton = viewAllButton();
+  const container = document.getElementById(viewAllButton); 
+  (viewAllButton);
+  
+
+
+  
