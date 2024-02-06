cb54a92f4e404af2611f3cf794dbc47c0eaf7608
Alexander Marston <amarstonbanyan@gmail.com>
2024-02-05 19:56:45 -0600

    updated files and file structure

--- js/home/home.js
+++
@@ -1,5 +0,0 @@
-function homePage() {
-    return `<div class="home-background">
-        <h1>Shop.CO</h1>
-    </div>`
-}
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// when you import your components remove reset this file before pushing your code.
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    <>
+      <SearchBar />
+    </>
+  );
+}
+
+export default App;
