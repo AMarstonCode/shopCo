fd737be487d2bdd64687c0f656f6fe473559f67f
THalsell <139520247+THalsell@users.noreply.github.com>
2024-02-07 19:31:29 -0600

    created a view all button

---
+++ src/components/THalsell/ViewAllButton.jsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+
+const ViewAllButton = () => {
+    return (
+        <>
+        <button class="view-all-button">View All</button>
+        </>
+    );
+};
+
+export default ViewAllButton
