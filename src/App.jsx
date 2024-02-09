afe024d38404544acefe376d36d5271b011d6f6a
aschallesa12 <schalles.williams.1212@gmail.com>
2024-02-08 21:01:43 -0500

    email input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 // when you import your components remove reset this file before pushing your code.
 import "./App.css";
 import SearchBar from "./components/SearchBar";
+import EmailInput from "./components/SWilliams/EmailInput";
 
 function App() {
   return (
     <>
       <SearchBar />
+      <br></br>
+      <EmailInput/>
     </>
   );
-}
+};
 
 export default App;
