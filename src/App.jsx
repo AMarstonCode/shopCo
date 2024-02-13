9bbbbf5c1ab0121d6cc87350bb2210e7bb656319
THalsell <139520247+THalsell@users.noreply.github.com>
2024-02-12 20:07:54 -0600

    created product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 // when you import your components remove reset this file before pushing your code.
 import "./App.css";
 import SearchBar from "./components/SearchBar";
-import ViewAllButton from "./components/Mblair/ViewAllButton";
 
 function App() {
   return (
     <>
       <SearchBar />
-      <ViewAllButton />
     </>
   );
 };
