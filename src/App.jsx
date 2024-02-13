74c9c46a823bf9a1ff13b265b2c86121e3688ac0
aschallesa12 <schalles.williams.1212@gmail.com>
2024-02-12 19:57:30 -0500

    fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 // when you import your components remove reset this file before pushing your code.
 import "./App.css";
 import SearchBar from "./components/SearchBar";
-import EmailInput from "./components/SWilliams/EmailInput";
 
 function App() {
   return (
     <>
       <SearchBar />
-      <br></br>
-      <EmailInput/>
     </>
   );
 };
