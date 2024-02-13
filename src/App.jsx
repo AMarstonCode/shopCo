b4f26eef20c6703851094490b98e25e840b3c336
Martez Carr <113547475+CodingCowboy30@users.noreply.github.com>
2024-02-13 12:53:02 -0600

    Product Card

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,6 @@ function App() {
       <SearchBar />
     </>
   );
-};
+}
 
 export default App;
---
+++ src/components/MCarr/ProductCard.jsx
@@ -0,0 +1,18 @@
+import './styles.css'
+
+
+const ProductCard = ({ productImage, productName, productPrice }) => {
+    return (
+      <>
+        <div className="product-card">
+          <img src={productImage} alt={productName} className="product-image" />
+          <div className="product-details">
+            <h3 className="product-name">{productName}</h3>
+            <p className="product-price">${productPrice}</p>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  export default ProductCard
